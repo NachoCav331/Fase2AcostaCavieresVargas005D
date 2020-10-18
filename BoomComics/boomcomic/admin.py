7723043517f04684edf080d421d290a83afdfe8f
nax ca <icavierescornejo13@gmail.com>
2020-10-18 19:10:33 -0300

    BoomComics: Sitio administrador

--- BoomComics/boomcomic/admin.py
+++ BoomComics/boomcomic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Tipo, Formato, Comic, Autor, ComicInstance
+
+admin.site.register(Tipo)
+admin.site.register(Formato)
+admin.site.register(Comic)
+admin.site.register(Autor)
+admin.site.register(ComicInstance)
